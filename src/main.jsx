63faef3e189f42ac4b3eb93c3c87cb5f9f4b6be7
hassanrazaprivate <w3755096@gmail.com>
2026-05-24 08:41:21 +0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import LeadCRM from './LeadCRM'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LeadCRM />
+  </React.StrictMode>
+)
